70064c00b817ea64dec354ece14e866212912cb0
Coy Hile <coy.hile@coyhile.com>
2020-04-30 23:43:08 -0400

    fix typo

--- chef/knife.rb
+++ chef/knife.rb
@@ -5,7 +5,7 @@ knife.yes = true
 # This can use an environment variable or can be hard-coded
 $chef_server_url = ENV.fetch('CHEF_SERVER_URL', 'https://chefserver.coyhile.com/organizations')
 $organization = ENV['CHEF_SERVER_ORG']
-$organization = ENV.fetch('CHEF_SERVER_ORG','chlabs'
+$organization = ENV.fetch('CHEF_SERVER_ORG','chlabs')
 veriy_api_cert false
 chef_server_url "#{$chef_server_url}/#{$organization}"
 node_name Etc.getpwuid.name
