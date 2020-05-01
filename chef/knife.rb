7c937518ef29f485f0a5f63d9b497d249c0e2836
Coy Hile <coy.hile@coyhile.com>
2020-04-30 20:58:36 -0400

    clean up knife.rb

--- chef/knife.rb
+++ chef/knife.rb
@@ -3,10 +3,9 @@ knife.vault_mode = 'client'
 knife.yes = true
 
 # This can use an environment variable or can be hard-coded
-# $chef_server_url = ENV.fetch('CHEF_SERVER_URL', 'https://chefserver.coyhile.com/organizations')
-# $organization = ENV['CHEF_SERVER_ORG']
-$chef_server_url = 'https://chefserver.coyhile.com/organizations'
-$organization = 'chl'
+$chef_server_url = ENV.fetch('CHEF_SERVER_URL', 'https://chefserver.coyhile.com/organizations')
+$organization = ENV['CHEF_SERVER_ORG']
+$organization = ENV.fetch('CHEF_SERVER_ORG','chlabs'
 veriy_api_cert false
 chef_server_url "#{$chef_server_url}/#{$organization}"
 node_name Etc.getpwuid.name
